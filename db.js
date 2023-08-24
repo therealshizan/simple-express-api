b33c04084e206daf0f927d03499224d0b80ca3ee
Shizan Shaikh <51877872+therealshizan@users.noreply.github.com>
2023-08-25 00:39:32 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'demodb',
-  password: 'abcd',
+  user: 'admin',
+  host: 'dpg-cjjql9fjbvhs73bjffv0-a',
+  database: 'demodb_yhme',
+  password: 'nUoX3AVI6zjfD3WOKWZd8eRzHTxtzRXq',
   port: 5432,
 });
 
