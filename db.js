d4c1a540fba0e7665450d753a3fe95ad12101a3f
Shizan Shaikh <51877872+therealshizan@users.noreply.github.com>
2023-08-25 00:42:24 +0530

    connectionString db.js

--- db.js
+++ db.js
@@ -1,11 +1,17 @@
 const { Pool } = require('pg');
 
+const connectionString = 'postgres://admin:nUoX3AVI6zjfD3WOKWZd8eRzHTxtzRXq@dpg-cjjql9fjbvhs73bjffv0-a.oregon-postgres.render.com/demodb_yhme'
+
 const pool = new Pool({
-  user: 'admin',
-  host: 'dpg-cjjql9fjbvhs73bjffv0-a',
-  database: 'demodb_yhme',
-  password: 'nUoX3AVI6zjfD3WOKWZd8eRzHTxtzRXq',
-  port: 5432,
+  // user: 'admin',
+  // host: 'dpg-cjjql9fjbvhs73bjffv0-a',
+  // database: 'demodb_yhme',
+  // password: 'nUoX3AVI6zjfD3WOKWZd8eRzHTxtzRXq',
+  // port: 5432,
+  connectionString: connectionString,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 });
 
 module.exports = pool;
